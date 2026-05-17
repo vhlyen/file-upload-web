734d92e0cc62ca56a8b9253ba10b2ced2ba6cd31
vhlyen <vohoanglamyen@gmail.com>
2026-05-17 21:45:32 +0700

    Updating

--- src/main/java/com/file_upload_web/WebConfig.java
+++ src/main/java/com/file_upload_web/WebConfig.java
@@ -1,13 +1,14 @@
 package com.file_upload_web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
     }
